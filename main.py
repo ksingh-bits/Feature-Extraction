efb3a55c9c1ca0836f75197b1c5cf677230b8cc6
ksingh-bits <ksingh@hyderabad.bits-pilani.ac.in>
2025-11-11 19:53:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ import os
 if 'all_stats' not in st.session_state:
     st.session_state.all_stats = pd.DataFrame()
 
-def calculate_statistical_data(reconstructed_signal, noise):
+def calculate_statistical_data(reconstructed_signal):
     params = {
         "Mean": np.mean(reconstructed_signal),
         "Median": np.median(reconstructed_signal),
@@ -331,6 +331,7 @@ with container:
         with col2:
             st.write(f"Loaded Files: {len(st.session_state.uploaded_files)}")
             st.write(f"Stored Records: {len(st.session_state.all_stats)}")
+
 
 
 
